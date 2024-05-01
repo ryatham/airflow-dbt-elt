69ec84583b8354a80ff0bbd86530f3d5756546e6
Ramesh <yatham.devops@gmail.com>
2024-05-01 15:08:17 +0530

    pushong dag changes

--- dags/src_hosts.py
+++ dags/src_hosts.py
@@ -1,67 +1,53 @@
-from datetime import timedelta
-from pendulum import datetime
+from airflow import DAG
+from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime, timedelta
 
-from airflow.decorators import dag
-from airflow.operators.bash import BashOperator
-from airflow_provider_hightouch.operators.hightouch import HightouchTriggerSyncOperator
-from airflow.utils.edgemodifier import Label
-from airflow.utils.trigger_rule import TriggerRule
+# Define default arguments for the DAG
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    'start_date': datetime(2024, 5, 1)
+}
 
+# Define Snowflake connection parameters
+snowflake_conn_id = 'snow_conn_id'  # Connection ID for Snowflake in Airflow
 
-# We're hardcoding the project directory value here for the purpose of the demo, but in a production
-# environment this would probably come from a config file and/or environment variables!
-DBT_PROJECT_DIR = "/usr/local/airflow/dbt"
+# Define the SQL query to execute in Snowflake
+snowflake_sql_query = """
+    SELECT * FROM AIRBNB.DEV.SRC_HOSTS limit 10
+"""
 
-# DBT_ENV = {
-#     "DBT_USER": "{{ conn.postgres.login }}",
-#     "DBT_ENV_SECRET_PASSWORD": "{{ conn.postgres.password }}",
-#     "DBT_HOST": "{{ conn.postgres.host }}",
-#     "DBT_SCHEMA": "{{ conn.postgres.schema }}",
-#     "DBT_PORT": "{{ conn.postgres.port }}",
-# }
+# Define the dbt command to run
+dbt_command = "dbt run --select src_hosts"
 
-
-@dag(
-    start_date=datetime(2024, 5, 1),
-    schedule_interval=None,
-    catchup=False,
-    doc_md=__doc__,
+# Define the DAG
+dag = DAG(
+    'snowflake_dbt_dag',
+    default_args=default_args,
+    description='DAG to connect to Snowflake and run dbt models',
+    schedule_interval='@daily',
+    catchup=False
 )
 
-def dbt_run_from_failure():
-    # This task loads the CSV files from dbt/data into the local Postgres database for the purpose of this demo.
-    # In practice, we'd usually expect the data to have already been loaded to the database.
-    dbt_seed = BashOperator(
-        task_id="dbt_seed",
-        bash_command=f"dbt seed --full-refresh --profiles-dir {DBT_PROJECT_DIR} --project-dir {DBT_PROJECT_DIR}",
-        # env=DBT_ENV,
-    )
+# Define a task to execute SQL query in Snowflake
+snowflake_task = SnowflakeOperator(
+    task_id='snowflake_task',
+    sql=snowflake_sql_query,
+    snowflake_conn_id=snowflake_conn_id,
+    dag=dag
+)
 
-    dbt_run = BashOperator(
-        task_id="dbt_run",
-        bash_command=f"dbt run --profiles-dir {DBT_PROJECT_DIR} --project-dir {DBT_PROJECT_DIR}",
-        # env=DBT_ENV,
-    )
+# Define a task to run dbt command
+dbt_task = BashOperator(
+    task_id='dbt_task',
+    bash_command=dbt_command,
+    dag=dag
+)
 
-    # Fill in the previous state artifacts needed to run from this point.
-    dbt_build_rerun = BashOperator(
-        task_id="dbt_build_rerun",
-        bash_command="dbt build --select result:error+ --defer --state <path/to/previous_state_artifacts>",
-        # env=DBT_ENV,
-        trigger_rule=TriggerRule.ALL_FAILED,
-    )
-
-    # Due to the ONE_SUCCESS rule, if one of the two upstream tasks succeed, this task will run.
-    sync_data_to_salesforce = HightouchTriggerSyncOperator(
-        task_id="sync_data_to_salesforce",
-        connection_id="hightouch",
-        sync_id=21,
-        synchronous=True,
-        trigger_rule=TriggerRule.ONE_SUCCESS,
-    )
-
-    # Explicitly setting this task as dependent on both upstream tasks.
-    dbt_seed >> dbt_run >> Label("Only if dbt run fails") >> dbt_build_rerun >> sync_data_to_salesforce
-    dbt_run >> Label("If it succeeds") >> sync_data_to_salesforce
-
-dag = dbt_run_from_failure()
+# Set task dependencies
+snowflake_task >> dbt_task
