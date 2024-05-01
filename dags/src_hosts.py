6504d197743c2144df1a971d5a7959bc859b80b1
Ramesh <yatham.devops@gmail.com>
2024-05-01 14:16:27 +0530

    pushong dag changes

--- dags/src_hosts.py
+++ dags/src_hosts.py
@@ -2,6 +2,9 @@ from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from datetime import datetime, timedelta
 
+
+DBT_PROJECT_DIR = "C:\Users\gullu\Desktop\course\airflow-dbt-elt\dbt"
+
 # Define default arguments for the DAG
 default_args = {
     'owner': 'airflow',
@@ -17,16 +20,20 @@ default_args = {
 dag = DAG(
     'src_hosts',
     default_args=default_args,
-    description='DAG to run dbt src_hosts model',
+    description='DAG to run a specific dbt src_hosts model',
     schedule_interval='@daily',
     catchup=False
 )
 
-# Define tasks for each dbt model
-src_hosts_task = BashOperator(
-    task_id='src_hosts_task',
+# Define a task to run a specific dbt model
+src_hosts = BashOperator(
+    task_id='src_hosts',
     bash_command='dbt run --model src_hosts',
     dag=dag
 )
 
-src_hosts_task
+src_hosts
+# Set task dependencies if needed
+# For example, if you have a previous task that needs to be completed before running the specific model
+# run_specific_model_task.set_upstream(earlier_task)
+
